b0192baebc0792356578830a2d6ea8f51c6cbf9a
Siddharth Borderwala <siddharthborderwala@gmail.com>
2023-07-17 14:49:04 +0530

    layout for mobile

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,12 +1,12 @@
 import MonacoEditor, { useMonaco } from '@monaco-editor/react'
 import Split from '@uiw/react-split'
 import classNames from 'classnames'
+import { useCallback, useEffect, useState } from 'react'
+import { Play, X } from '@phosphor-icons/react'
 import { useEditorThemeValue, useThemeValue } from '../atoms/settings'
 import { useCode } from '../atoms/code'
 import { ConsoleEntryType, useWriteToConsole } from '../atoms/console'
-import { useCallback, useEffect } from 'react'
 import { ConsoleView } from './console-view'
-import { Play, X } from '@phosphor-icons/react'
 import { Loader } from './loader'
 import { generateThemeConfig, getThemeType } from '../themes'
 
@@ -16,6 +16,11 @@ export const Editor = () => {
   const editorTheme = useEditorThemeValue()
   // do not remove as it is used in eval
   const [writeToConsole, clearConsole] = useWriteToConsole()
+  const [splitMode, setSplitMode] = useState<'horizontal' | 'vertical'>(() => {
+    return matchMedia?.('(max-width: 640px)').matches
+      ? 'vertical'
+      : 'horizontal' ?? 'horizontal'
+  })
 
   const handleExec = useCallback(
     (code: string) => {
@@ -76,57 +81,88 @@ export const Editor = () => {
     })
   }, [monaco, editorTheme])
 
+  useEffect(() => {
+    // listen to window resize and change split mode using matchMedia
+    const mediaQuery = matchMedia('(max-width: 640px)')
+    const handleChange = (e: MediaQueryListEvent) => {
+      setSplitMode(e.matches ? 'vertical' : 'horizontal')
+    }
+    mediaQuery.addEventListener('change', handleChange)
+    return () => {
+      mediaQuery.removeEventListener('change', handleChange)
+    }
+  }, [])
+
   return (
     <Split
       className="border-b border-b-gray-300 dark:border-gray-700 h-full"
-      mode="horizontal"
+      mode={splitMode}
       renderBar={({ onMouseDown, className, ...props }) => {
         return (
           <div
             {...props}
-            className={classNames(
-              className,
-              '!bg-transparent !shadow-none !w-[6px]'
-            )}
+            className={classNames(className, '!bg-transparent !shadow-none', {
+              '!w-[6px]': splitMode === 'horizontal',
+              '!h-[6px]': splitMode === 'vertical'
+            })}
           >
             <div
               onMouseDown={onMouseDown}
-              className="bg-gray-200/20 !w-[6px] dark:bg-gray-700/20 backdrop-blur-3xl !shadow-none group/dragger"
+              className={classNames(
+                'bg-gray-200/20 dark:bg-gray-700/20 backdrop-blur-3xl !shadow-none group/dragger',
+                {
+                  '!w-[6px]': splitMode === 'horizontal',
+                  '!h-[6px]': splitMode === 'vertical'
+                }
+              )}
             >
               <div
                 onMouseDown={onMouseDown}
-                className="w-[1px] translate-x-[2.5px] h-full transition-colors bg-gray-300 group-hover/dragger:bg-blue-500/30 dark:bg-gray-700 group-hover/dragger:dark:bg-blue-700/30"
+                className={classNames(
+                  'transition-colors bg-gray-300 group-hover/dragger:bg-blue-500/30 dark:bg-gray-700 group-hover/dragger:dark:bg-blue-700/30',
+                  {
+                    'w-[1px] translate-x-[2.5px] h-full':
+                      splitMode === 'horizontal',
+                    'h-[1px] translate-y-[2.5px] w-full':
+                      splitMode === 'vertical'
+                  }
+                )}
               />
             </div>
           </div>
         )
       }}
     >
-      <MonacoEditor
-        loading={
-          <div className="flex flex-col gap-2 items-center justify-center">
-            <Loader theme={theme} />
-            <p className="text-gray-700 dark:text-gray-300">Loading Editor</p>
-          </div>
-        }
-        height="100%"
-        language="javascript"
-        className="w-3/5 overflow-x-auto overflow-y-auto font-mono"
-        theme={editorTheme}
-        options={{
-          fontSize: 14,
-          fontFamily: 'monospace',
-          fontLigatures: true,
-          minimap: {
-            enabled: false
+      <div className="w-full h-2/3 sm:h-full sm:w-3/5 ">
+        <MonacoEditor
+          loading={
+            <div className="flex flex-col gap-2 items-center justify-center">
+              <Loader theme={theme} />
+              <p className="text-gray-700 dark:text-gray-300">Loading Editor</p>
+            </div>
           }
-        }}
-        value={code}
-        onChange={(value) => {
-          setCode(value ?? '')
-        }}
-      />
-      <div className="w-2/5 overflow-x-auto" id="console-container">
+          height="100%"
+          language="javascript"
+          className="overflow-x-auto overflow-y-auto font-mono"
+          theme={editorTheme}
+          options={{
+            fontSize: 14,
+            fontFamily: 'monospace',
+            fontLigatures: true,
+            minimap: {
+              enabled: false
+            }
+          }}
+          value={code}
+          onChange={(value) => {
+            setCode(value ?? '')
+          }}
+        />
+      </div>
+      <div
+        className="w-full h-1/3 sm:h-full sm:w-2/5 overflow-x-auto"
+        id="console-container"
+      >
         <div className="py-1 px-2 border-b border-gray-200 dark:border-gray-700 bg-gray-100 dark:bg-[#292A2D] flex items-center justify-between">
           <p className="font-mono text-xs text-gray-700 dark:text-gray-50">
             main.js
